8d940e522b0165472828546ca3b6bf4c750587a7
Valeria Saparina <kzn.2003valeria@gmail.com>
2023-11-17 19:13:22 +0300

    implement an authorization verification filter

---
+++ src/main/java/com/example/movieproject/Filters/IsAuthFilter.java
@@ -0,0 +1,26 @@
+package com.example.movieproject.Filters;
+
+import com.example.movieproject.Helpers.Helper;
+import com.example.movieproject.Helpers.Params;
+import com.example.movieproject.Helpers.services.UserService;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class IsAuthFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        UserService userService = (UserService) req.getServletContext().getAttribute(Params.USER_SERVICE);
+        if (userService.isAuth(req)) {
+            chain.doFilter(req, res);
+        } else {
+            Helper.redirect(res, req, "/auth");
+        }
+
+    }
+}
